fb074ae1b696571ed0e2991afaf05ec82b015ce2
Felipe Frizzo <felipefrizzo@gmail.com>
2016-06-23 02:12:40 -0300

    Update install-atom-plugins.sh

--- install-atom-plugins.sh
+++ install-atom-plugins.sh
@@ -1,7 +1,37 @@
+#!/bin/bash
 echo "\nInstalling atom plugins"
-apm install autocomplete-glsl
+
+apm install environment
+apm install linter
+
+apm install autocomplete-go
+apm install builder-go
+apm install go-config
+apm install go-get
 apm install go-oracle
 apm install go-plus
+apm install gorename
+apm install gofmt
+apm install gometalinter-linter
+apm install navigator-godef
+apm install tester-go
+
+apm install language-babel
+apm install language-docker
+apm install language-protobuf
+apm install language-javascript-jsx
+
 apm install markdown-preview-plus
 apm install markdown-writer
+
 apm install mathjax-wrapper
+
+apm install es6-javascript
+apm install angularjs
+apm install react
+apm install redux-snippets
+
+apm install color-picker
+apm install tab-numbers
+apm install file-icons
+apm install highlight-line
