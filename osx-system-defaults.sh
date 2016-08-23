fc8584607b733109ae7a9a6affb8280ab2366528
Felipe Frizzo <felipefrizzo@gmail.com>
2016-08-23 15:52:08 -0300

    Update osx-system-defaults.sh

--- osx-system-defaults.sh
+++ osx-system-defaults.sh
@@ -1,11 +1,12 @@
-echo "\nSet OsX system defaults"
+echo ""
+echo "Set OsX system defaults"
 # ==============================================
 # Set energy preferences
 # ==============================================
 IS_LAPTOP=`/usr/sbin/system_profiler SPHardwareDataType | grep "Model Identifier" | grep "Book"`
 if [[ "$IS_LAPTOP" != "" ]]; then
-    pmset -b sleep 20 disksleep 15 displaysleep 10 halfdim 1
-    pmset -c sleep 0 disksleep 0 displaysleep 30 halfdim 1
+    pmset -b sleep 20 disksleep 10 displaysleep 5 halfdim 1
+    pmset -c sleep 0 disksleep 15 displaysleep 10 halfdim 1
 
     # Menu bar: show remaining battery time (on pre-10.8); hide percentage
     defaults write com.apple.menuextra.battery ShowPercent -string "YES"
