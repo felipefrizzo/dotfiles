b6e4a7fca637919316c9d45bff012cff986e1819
Felipe Frizzo <felipefrizzo@gmail.com>
2017-04-12 23:55:43 -0300

    Add atom plugin

--- install-atom-plugins.sh
+++ install-atom-plugins.sh
@@ -22,7 +22,7 @@ apm install language-javascript-jsx
 apm install es6-javascript
 apm install angularjs
 apm install angularjs-styleguide-snippets
-
+apm install autocomplete-modules
 
 apm install markdown-preview-plus
 apm install markdown-writer
