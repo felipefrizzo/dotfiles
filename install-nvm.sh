c0b44c1c16ad35ac42d08941a3c08e45edce2fb9
Felipe Frizzo <felipefrizzo@gmail.com>
2017-08-21 17:30:14 -0300

    Fixed nvm linux path

--- install-nvm.sh
+++ install-nvm.sh
@@ -3,4 +3,4 @@ echo ""
 echo  "Installing NVM"
 
 curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh -o $HOME/install_nvm.sh
-sudo sh $HOME/install-nvm.shs
+sudo sh $HOME/install-nvm.sh
