f5057a6b8f2959b54c1683ff910b10c25f2e0ff9
Felipe Frizzo <felipefrizzo@gmail.com>
2017-08-21 17:47:56 -0300

    Change java version

--- install-java.sh
+++ install-java.sh
@@ -4,6 +4,6 @@ echo "Installing Java"
 
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt update
-sudo apt install -y oracle-java9-installer
+sudo apt install -y oracle-java8-installer
 
 sudo update-alternatives --config java
