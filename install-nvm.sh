782fc7e5ed806d19654e076f7a1539f6d32dee14
Felipe Frizzo <felipefrizzo@gmail.com>
2017-08-22 08:30:29 -0300

    fixed nvm installation

--- install-nvm.sh
+++ install-nvm.sh
@@ -2,5 +2,5 @@
 echo ""
 echo  "Installing NVM"
 
-curl -sL https://raw.githubusercontent.com/creationix/nvm/v0.31.0/install.sh -o $HOME/install_nvm.sh
-sudo sh $HOME/install-nvm.sh
+VERSION='v0.33.2'
+curl -o- 'https://raw.githubusercontent.com/creationix/nvm/'$VERSION'/install.sh' | bash
