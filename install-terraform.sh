5494009baf2ddca3a95468c1839849f1c32861d0
Felipe Frizzo <felipefrizzo@gmail.com>
2017-08-22 08:31:11 -0300

    fixed terraform version

--- install-terraform.sh
+++ install-terraform.sh
@@ -4,8 +4,8 @@ echo "Installing Terraform"
 mkdir $HOME/terraform
 
 # Download Terraform. URI: https://www.terraform.io/downloads.html
-VERSION='0.9.4'
+VERSION='0.10.2'
 RELEASE='terraform_linux_amd64.zip'
-curl -L 'https://releases.hashicorp.com/terraform/'$VERSION'/terraform_'$VERSION'_linux_amd64.zip' -o $HOME/terraform/$RELEASE
+wget 'https://releases.hashicorp.com/terraform/'$VERSION'/terraform_'$VERSION'_linux_amd64.zip?_ga=2.267933449.605624787.1503400238-989872445.1503400238' -O $HOME/terraform/$RELEASE
 # Unzip and install
 unzip $HOME/terraform/$RELEASE
