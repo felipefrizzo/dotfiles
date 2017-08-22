ac781bbcc8faea95c8edfe21d3cc98f9f5701d8c
Felipe Frizzo <felipefrizzo@gmail.com>
2017-08-22 08:30:57 -0300

    fixed rubymine installation

--- install-rubymine.sh
+++ install-rubymine.sh
@@ -6,5 +6,5 @@ RELEASE='RubyMine'
 VERSION='2017.2.1'
 
 # Downloads
-curl -L 'https://download.jetbrains.com/ruby/'$RELEASE'-'$VERSION'.tar.gz' -O '$HOME/'$RELEASE'-'$VERSION'.tar.gz'
-tar -xvf '$HOME/'$RELEASE'-'$VERSION'.tar.gz'
+wget 'https://download.jetbrains.com/ruby/'$RELEASE'-'$VERSION'.tar.gz' -O $HOME'/'$RELEASE'-'$VERSION'.tar.gz'
+tar -xvf $HOME'/'$RELEASE'-'$VERSION'.tar.gz'
