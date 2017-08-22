b18e3e5e2efce9d89bd3f4c3733422af59c25323
Felipe Frizzo <felipefrizzo@gmail.com>
2017-08-22 08:38:59 -0300

    Update docker compose version and add docker machine

--- install-docker.sh
+++ install-docker.sh
@@ -13,6 +13,11 @@ sudo add-apt-repository \
 sudo apt update
 sudo apt install -y docker-ce
 
+DOCKER_COMPOSE_VERSION='1.14.0'
 sudo -i
-curl -L https://github.com/docker/compose/releases/download/1.12.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+curl -L 'https://github.com/docker/compose/releases/download/'$DOCKER_COMPOSE_VERSION'/docker-compose-'`uname -s`-`uname -m` > /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
+
+DOCKER_MACHINE_VERSION='v0.12.2'
+curl -L 'https://github.com/docker/machine/releases/download/'$DOCKER_MACHINE_VERSION'/docker-machine-'`uname -s`-`uname -m` >/usr/local/bin/docker-machine
+chmod +x /usr/local/bin/docker-machine
